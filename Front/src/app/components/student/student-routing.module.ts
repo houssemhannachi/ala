38e2a4561fa2da9d288834b941dca9fe14f6f492
Houssem Hannachi <houssem.hannachi@euranova.eu>
2025-04-15 11:28:28 +0100

    add review

--- Front/src/app/components/student/student-routing.module.ts
+++ Front/src/app/components/student/student-routing.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { StudentComponent } from './student.component';
-import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
-import { StudentMessageComponent } from './student-message/student-message.component';
-import { StudentQaComponent } from './student-qa/student-qa.component';
-import { StudentQuizComponent } from './student-quiz/student-quiz.component';
-import { StudentOrderHistoryComponent } from './student-order-history/student-order-history.component';
-import { StudentWishlistComponent } from './student-wishlist/student-wishlist.component';
-import { StudentCoursesComponent } from './student-courses/student-courses.component';
-import { StudentProfileComponent } from './student-profile/student-profile.component';
-import { ViewInvoiceComponent } from './view-invoice/view-invoice.component';
-import { StudentReviewsComponent } from './student-reviews/student-reviews.component';
-import { StudentReferralComponent } from './student-referral/student-referral.component';
-import { StudentTicketsComponent } from './student-tickets/student-tickets.component';
-import { StudentQuizDetailsComponent } from './student-quiz-details/student-quiz-details.component';
-import { AfficheReservationComponent } from './affiche-reservation/affiche-reservation.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {StudentComponent} from './student.component';
+import {StudentDashboardComponent} from './student-dashboard/student-dashboard.component';
+import {StudentMessageComponent} from './student-message/student-message.component';
+import {StudentQuizComponent} from './student-quiz/student-quiz.component';
+import {StudentOrderHistoryComponent} from './student-order-history/student-order-history.component';
+import {StudentWishlistComponent} from './student-wishlist/student-wishlist.component';
+import {StudentCoursesComponent} from './student-courses/student-courses.component';
+import {StudentProfileComponent} from './student-profile/student-profile.component';
+import {ViewInvoiceComponent} from './view-invoice/view-invoice.component';
+import {StudentReviewsComponent} from './student-reviews/student-reviews.component';
+import {StudentReferralComponent} from './student-referral/student-referral.component';
+import {StudentTicketsComponent} from './student-tickets/student-tickets.component';
+import {StudentQuizDetailsComponent} from './student-quiz-details/student-quiz-details.component';
+import {AfficheReservationComponent} from './affiche-reservation/affiche-reservation.component';
 
-import { EventListComponent } from './event-list/event-list.component';
+import {EventListComponent} from './event-list/event-list.component';
+import {StudentEventDetailsComponent} from "./student-event-details/student-event-details.component";
 
 const routes: Routes = [
   {
@@ -27,7 +27,7 @@ const routes: Routes = [
       {
         path: 'students/reserve/:eventId',
         component: AfficheReservationComponent,
-    },
+      },
       {
 
         path: 'student-view',
@@ -45,20 +45,21 @@ const routes: Routes = [
         component: ViewInvoiceComponent,
       },
 
-      { path: 'student-dashboard', component: StudentDashboardComponent },
-      { path: 'events-list', component:  EventListComponent},
-      { path: 'student-quiz', component: StudentQuizComponent },
-      { path: 'student-message', component: StudentMessageComponent },
+      {path: 'student-dashboard', component: StudentDashboardComponent},
+      {path: 'events-list', component: EventListComponent},
+      {path: 'student-quiz', component: StudentQuizComponent},
+      {path: 'student-message', component: StudentMessageComponent},
       {
         path: 'student-order-history',
         component: StudentOrderHistoryComponent,
       },
-      { path: 'student-wishlist', component: StudentWishlistComponent },
-      { path: 'student-courses', component: StudentCoursesComponent },
-      { path: 'student-quiz-details', component: StudentQuizDetailsComponent },
-      { path: 'student-reviews', component: StudentReviewsComponent },
-      { path: 'student-referral', component: StudentReferralComponent },
-      { path: 'student-tickets', component: StudentTicketsComponent },
+      {path: 'student-wishlist', component: StudentWishlistComponent},
+      {path: 'student-courses', component: StudentCoursesComponent},
+      {path: 'student-quiz-details', component: StudentQuizDetailsComponent},
+      {path: 'student-event-details/:id', component: StudentEventDetailsComponent},
+      {path: 'student-reviews', component: StudentReviewsComponent},
+      {path: 'student-referral', component: StudentReferralComponent},
+      {path: 'student-tickets', component: StudentTicketsComponent},
       {
         path: 'settings',
         loadChildren: () =>
@@ -72,4 +73,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class StudentRoutingModule {}
+export class StudentRoutingModule {
+}
--- Front/src/app/components/student/student.module.ts
+++ Front/src/app/components/student/student.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { StudentRoutingModule } from './student-routing.module';
-import { StudentComponent } from './student.component';
-import { FeatherIconModule } from 'src/app/shared/module/feather.module';
-import { SharedModule } from 'src/app/shared/module/shared.module';
-import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
-import { StudentMessageComponent } from './student-message/student-message.component';
-import { StudentOrderHistoryComponent } from './student-order-history/student-order-history.component';
-import { RouterModule } from '@angular/router';
-import { StudentQaComponent } from './student-qa/student-qa.component';
-import { StudentProfileComponent } from './student-profile/student-profile.component';
-import { StudentWishlistComponent } from './student-wishlist/student-wishlist.component';
-import { StudentCoursesComponent } from './student-courses/student-courses.component';
-import { StudentQuizComponent } from './student-quiz/student-quiz.component';
-import { StudentReviewsComponent } from './student-reviews/student-reviews.component';
-import { StudentReferralComponent } from './student-referral/student-referral.component';
-import { StudentTicketsComponent } from './student-tickets/student-tickets.component';
-import { StudentQuizDetailsComponent } from './student-quiz-details/student-quiz-details.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AfficheReservationComponent } from './affiche-reservation/affiche-reservation.component';
-import { EventListComponent } from './event-list/event-list.component';
+import {StudentRoutingModule} from './student-routing.module';
+import {StudentComponent} from './student.component';
+import {FeatherIconModule} from 'src/app/shared/module/feather.module';
+import {SharedModule} from 'src/app/shared/module/shared.module';
+import {StudentDashboardComponent} from './student-dashboard/student-dashboard.component';
+import {StudentMessageComponent} from './student-message/student-message.component';
+import {StudentOrderHistoryComponent} from './student-order-history/student-order-history.component';
+import {RouterModule} from '@angular/router';
+import {StudentQaComponent} from './student-qa/student-qa.component';
+import {StudentProfileComponent} from './student-profile/student-profile.component';
+import {StudentWishlistComponent} from './student-wishlist/student-wishlist.component';
+import {StudentCoursesComponent} from './student-courses/student-courses.component';
+import {StudentQuizComponent} from './student-quiz/student-quiz.component';
+import {StudentReviewsComponent} from './student-reviews/student-reviews.component';
+import {StudentReferralComponent} from './student-referral/student-referral.component';
+import {StudentTicketsComponent} from './student-tickets/student-tickets.component';
+import {StudentQuizDetailsComponent} from './student-quiz-details/student-quiz-details.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AfficheReservationComponent} from './affiche-reservation/affiche-reservation.component';
+import {EventListComponent} from './event-list/event-list.component';
+import {StudentEventDetailsComponent} from "./student-event-details/student-event-details.component";
 
 
 @NgModule({
@@ -38,6 +39,7 @@ import { EventListComponent } from './event-list/event-list.component';
     StudentReferralComponent,
     StudentTicketsComponent,
     StudentQuizDetailsComponent,
+    StudentEventDetailsComponent,
     AfficheReservationComponent,
     EventListComponent
   ],
@@ -46,9 +48,10 @@ import { EventListComponent } from './event-list/event-list.component';
     StudentRoutingModule,
     FeatherIconModule,
     SharedModule,
-    RouterModule ,
+    RouterModule,
     FormsModule,
     ReactiveFormsModule
   ]
 })
-export class StudentModule { }
+export class StudentModule {
+}
